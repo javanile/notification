fd259d095d05e90d83cf16b631c333b3f35dad01
francesco <info.francescobianco@gmail.com>
2023-02-06 18:37:08 +0100

    First commit

--- tests/bare/trigger-live-test.sh
+++ tests/bare/trigger-live-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -d "@tests/fixtures/payload.json" https://notification-1.herokuapp.com/
+curl -d @tests/fixtures/payload.json https://notification-1.herokuapp.com/send_push_notification.php
